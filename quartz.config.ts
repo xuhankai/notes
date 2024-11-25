4277e3b28ca44a4f803b98c07fd497449d27c042
Kevin Xu <masterop589@gmail.com>
2024-11-24 23:54:09 -0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import { Staticrypt } from "./quartz/password"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Notes🖊️",
+    pageTitle: "Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
